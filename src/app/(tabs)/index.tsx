1cdef8b7ba6629cea5cf452f61f6e90932ae47d7
Sylvanus <sepehr.haghdoust@gmail.com>
2024-03-19 11:59:36 +0330

    replacing hardCode

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,22 +3,20 @@ import { StyleSheet } from "react-native";
 import EditScreenInfo from "@/src/components/EditScreenInfo";
 import { Text, View } from "@/src/components/Themed";
 import Colors from "@/src/constants/Colors";
+import products from "../../../assets/data/products";
 
+const product = products[0];
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Pizza Peperoni</Text>
-      <Text style={styles.price}>$12.99</Text>
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  container: {},
   title: {
     fontSize: 18,
     fontWeight: "600",
